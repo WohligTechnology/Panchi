e62b79d01ba7c16ad6f5438c51d5718b75f9c800
TusharSachde <tushar@wohlig.com>
2015-10-28 16:18:45 +0530

    Search k liye mocha

--- NoteShare/TableViewController.swift
+++ NoteShare/TableViewController.swift
@@ -8,27 +8,42 @@
 
 import UIKit
 
-class TableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+@available(iOS 8.0, *)
+class TableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchResultsUpdating {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    let notes = ["BOTANY: THE CLASSIFICATION OF MANY BOX", "Physics Theory Links", "Maths", "Computer Science", "Graphics"]
+    var filteredNotes = [String]()
+    var resultSearchController = UISearchController()
+
     
     var textArray: NSMutableArray! = NSMutableArray()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.textArray.addObject("BOTANY: THE CLASSIFICATION OF MANY BOX")
+        self.resultSearchController = UISearchController(searchResultsController: nil)
+        self.resultSearchController.searchResultsUpdater = self
+        self.resultSearchController.dimsBackgroundDuringPresentation = false
+        self.resultSearchController.searchBar.sizeToFit()
         
-        self.textArray.addObject("Physics Theory Links")
+        self.tableView.tableHeaderView = self.resultSearchController.searchBar
+        self.tableView.reloadData()
         
-        self.textArray.addObject("Maths")
-        
-        self.textArray.addObject("Computer Science")
-        
-        self.textArray.addObject("Graphics")
-        
+        //Automate the height of rows
         self.tableView.rowHeight = UITableViewAutomaticDimension
         self.tableView.estimatedRowHeight = 44.0
+        
+        //Clear Empty Cells
+        var backgroundView = UIView(frame: CGRectZero)
+        self.tableView.tableFooterView = backgroundView
+        self.tableView.backgroundColor = UIColor.clearColor()
+        
+        //Show search on scroll
+        self.tableView.setContentOffset(CGPoint(x: 0,y: 44), animated: true)
+        self.searchBar.resignFirstResponder()
         
     }
     
@@ -37,23 +52,53 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
         // Dispose of any resources that can be recreated.
     }
     
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return self.textArray.count
-        
+        if (self.resultSearchController.active)
+        {
+            return self.filteredNotes.count
+        }
+        else
+        {
+            return self.notes.count
+        }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        var cell: UITableViewCell = (self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell?)!
+        //Put data into cells
+        var cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell?
         
-        cell.textLabel?.text = self.textArray.objectAtIndex(indexPath.row) as? String
+        //Set hover color for the Cells
         var bgView : UIView = UIView()
         bgView.backgroundColor = UIColor(red: 255.0/255.0, green: 172.0/255.0, blue: 175.0/255.0, alpha: 1.0)
-        cell.selectedBackgroundView = bgView
-        return cell
+        cell!.selectedBackgroundView = bgView
         
+        if (self.resultSearchController.active)
+        {
+            cell!.textLabel?.text = self.filteredNotes[indexPath.row]
+            
+            return cell!
+        }
+        else
+        {
+            cell!.textLabel?.text = self.notes[indexPath.row]
+            
+          return cell!
+        }
+    
     }
-    
-    
+
+    func updateSearchResultsForSearchController(searchController: UISearchController)
+    {
+        self.filteredNotes.removeAll(keepCapacity: false)
+        
+        let searchPredicate = NSPredicate(format: "SELF CONTAINS[c] %@", searchController.searchBar.text!)
+        let array = (self.notes as NSArray).filteredArrayUsingPredicate(searchPredicate)
+        self.filteredNotes = array as! [String]
+        
+        self.tableView.reloadData()
+    }
+
 }
