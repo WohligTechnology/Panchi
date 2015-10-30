0bf5c8102a2ae53b8d7155fe7791e28a476ba494
TusharSachde <tushar@wohlig.com>
2015-10-30 18:36:24 +0530

    With Working pods

--- NoteShare/NoteShare-Bridging-Header.h
+++ NoteShare/NoteShare-Bridging-Header.h
@@ -5,7 +5,7 @@
 //  Copyright © 2015 Tushar Sachde. All rights reserved.
 //
 
-#import "MGSwipeTableCell.h"
-#import "MGSwipeButton.h"
-#import "MZAppearance.h"
-#import "MZFormSheetPresentationViewController.h"
+#import <MGSwipeTableCell/MGSwipeTableCell.h>
+#import <MGSwipeTableCell/MGSwipeButton.h>
+#import <MZAppearance/MZAppearance.h>
+#import <MZFormSheetPresentationController/MZFormSheetPresentationViewController.h>
